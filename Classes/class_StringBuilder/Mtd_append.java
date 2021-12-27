c6402b327c81cd803d2bafaa41193cc69aa8f305
Gustavo Lima <gustavolrs18@gmail.com>
2021-12-27 17:59:33 -0300

    Pequenas Alterações.

--- Classes/class_StringBuilder/Mtd_append.java
+++ Classes/class_StringBuilder/Mtd_append.java
@@ -3,9 +3,9 @@ package class_StringBuilder;
 public class Mtd_append {
 	public static void main(String[] args) {
 	
-	//o metodo append recebe como argumento uma string que será concatenada pela qual está chamando o método.
-	//serve para otimizar o uso de memória pois concatenar com String aloca espaços de memória desnecessariamente
-	//o que em alguns casos pode causar muita diferença de tempo do processamento
+	//o metodo append recebe como argumento uma string que sera concatenada pela qual esta chamando o metodo.
+	//serve para otimizar o uso de memória pois concatenar com String aloca espasos de memoria desnecessariamente
+	//o que em alguns casos pode causar muita diferenca de tempo do processamento
 
 	StringBuilder nomes = new StringBuilder();
 		nomes.append("Eddward > ").append("Daniel > ").append("Lais > ").append("Claudia > ").append("Jonas > ").append("Arthur > ")
